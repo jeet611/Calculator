0232b64ff8c65c5ecea1f8f805b678fa3b7c4dfe
jeet2610 <63009294+jeet2610@users.noreply.github.com>
2022-10-02 15:11:25 +0530

    Update calc.c

--- calc.c
+++ calc.c
@@ -3,7 +3,7 @@ int main()
 {  
     // declare local variables  
     char opt;  
-    int n1, n2,n3;   
+    int n1, n2;   
     float res;  
     printf (" Choose an operator(+, -, *, /) to perform the operation in C Calculator \n ");  
     scanf ("%c", &opt); // take an operator  
@@ -41,8 +41,21 @@ int main()
             printf (" Subtraction of %d and %d is: %.2f", n1, n2, res);  
             break;  
               
-       
-        default:  
+        case '*':  
+            res = n1 * n2; // multiply two numbers  
+            printf (" Multiplication of %d and %d is: %.2f", n1, n2, res);  
+            break;            
+          
+        case '/':  
+            if (n2 == 0)   // if n2 == 0, take another number  
+            {  
+                printf (" \n Divisor cannot be zero. Please enter another value ");  
+                scanf ("%d", &n2);        
+                }  
+            res = n1 / n2; // divide two numbers  
+            printf (" Division of %d and %d is: %.2f", n1, n2, res);  
+            break;  
+        default:  /* use default to print default message if any condition is not satisfied */  
             printf (" Something is wrong!! Please check the options ");               
     }  
     return 0;  
